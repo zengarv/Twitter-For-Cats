0ad623f29fd537a782dd9a7b06532848a593b497
zengarv <garv.jain835@gmail.com>
2022-10-14 20:33:30 +0530

    Added ability to fetch from raw csv file on github

--- tests.py
+++ tests.py
@@ -1,11 +1,13 @@
+from traceback import print_tb
 import pandas as pd
 import random
 
 # t = pd.read_csv('tweets.csv', delimiter='`')
 # print(t.sample())
 
-usernames = pd.read_csv('rngusernames.csv')
+usernames = pd.read_csv('https://raw.githubusercontent.com/zengarv/Twitter-For-Cats/master/rngusernames.csv')
 u = (usernames.sample(n=1))
-for _ in range(10): print(usernames.at[random.randint(0, len(usernames.index)-1), 'Adj'])
+print(usernames.describe())
+for _ in range(10): print(usernames.at[random.randint(0, len(usernames.index)-1), 'Handles'])
 
 
